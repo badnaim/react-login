522fe73256345af091089046d5acd3954e83a36b
badnaim <batnyaam.tumurbat@gmail.com>
2023-01-12 09:47:51 +0800

    b

--- jan-11/src/App.js
+++ jan-11/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import React, { useState } from "react";
+import { data } from "./util/data";
 import Profile from "./components/Profile";
 import WelcomePage from "./components/WelcomePage";
-import userList from "./util/data";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(userList);
+  const [userData, setUserData] = useState(data);
 
   function loginHandler(userName, password) {
     console.log("loginHandler is running");
     console.log("username: ", userName);
     console.log("password: ", password);
-    userList.map((user) => {
-      if (userName == user.userName && password == user.password) {
+    userData.map((user) => {
+      if (userName === user.userName && password === user.password) {
         setIsLoggedIn(true);
       } else {
         console.error("wrong password or username!");
--- jan-11/src/util/data.js
+++ jan-11/src/util/data.js
@@ -1,9 +1,7 @@
-export default function userList() {
-  return (data = [
-    { userName: "batnyam", password: "123" },
-    { userName: "bold", password: "1234" },
-    { userName: "bat", password: "12345" },
-    { userName: "och", password: "123456" },
-    { userName: "andy", password: "asd" },
-  ]);
-}
+export const data = [
+  { userName: "batnyam", password: "123" },
+  { userName: "bold", password: "1234" },
+  { userName: "bat", password: "12345" },
+  { userName: "och", password: "123456" },
+  { userName: "andy", password: "asd" },
+];
